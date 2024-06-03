6deca2a5f9136937508760ecb535dd834fbe1d0b
Alex Neutzler <alex.neutzler@melted-cloud.de>
2024-06-03 17:24:31 +0200

    added beep_ok and beep_error

--- beep_server.py
+++ beep_server.py
@@ -8,5 +8,17 @@ def beep():
     os.system('beep')
     return "Beep triggered!", 200
 
+@app.route('/beep_ok', methods=['GET'])
+def beep_ok():
+    # Zwei kurze Beep-Signale für "OK"
+    os.system('beep -f 1000 -l 100 && sleep 0.1 && beep -f 1000 -l 100')
+    return "OK beep triggered!", 200
+
+@app.route('/beep_error', methods=['GET'])
+def beep_error():
+    # Ein längeres, tieferes Beep-Signal für Fehler
+    os.system('beep -f 500 -l 500')
+    return "Error beep triggered!", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
